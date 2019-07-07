61a3323a221fb303b5377beea3c0a4ef0c480c1e
infsolution <clsinfsolution@gmail.com>
2019-07-07 17:38:01 -0300

    add module web

--- detect/detect/settings.py
+++ detect/detect/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap',
+    'fontawesome',
     'rest_framework',
     'api',
+    'webapp',
 ]
 
 MIDDLEWARE = [
